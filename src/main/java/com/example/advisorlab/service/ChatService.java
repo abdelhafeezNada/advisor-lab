7a78d2334838cb39fc764f4554105922abd8ff21
abdelhafeezNada <abdelhafeez0nada@gmail.com>
2026-09-25 17:31:48 +0300

    create chatService with chatClient injected

---
+++ src/main/java/com/example/advisorlab/service/ChatService.java
@@ -0,0 +1,44 @@
+package com.example.advisorlab.service;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ChatService {
+
+  private final ChatClient chatClient;
+
+  public ChatService(ChatClient chatClient) {
+    this.chatClient = chatClient;
+  }
+
+  /**
+   * ChatClient
+   * │
+   * ▼
+   * prompt()
+   * │
+   * ▼
+   * user(message)
+   * │
+   * ▼
+   * call()
+   * │
+   * ▼
+   * ChatModel
+   * │
+   * ▼
+   * Ollama
+   * │
+   * ▼
+   * content()
+   */
+  public String chat(String message) {
+
+    return chatClient
+        .prompt()
+        .user(message)
+        .call()
+        .content();
+  }
+}
