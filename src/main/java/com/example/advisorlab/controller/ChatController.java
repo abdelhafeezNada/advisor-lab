0fa6c643c43d7a81b4a8450a7b0ae9da7143bdd6
abdelhafeezNada <abdelhafeez0nada@gmail.com>
2026-09-25 17:37:11 +0300

    inject ChatService in ChatController

--- src/main/java/com/example/advisorlab/controller/ChatController.java
+++ src/main/java/com/example/advisorlab/controller/ChatController.java
@@ -3,8 +3,16 @@ package com.example.advisorlab.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.advisorlab.service.ChatService;
+
 @RestController
 @RequestMapping("/api/chat")
 public class ChatController {
+
+  private final ChatService chatService;
+
+  public ChatController(ChatService chatService) {
+    this.chatService = chatService;
+  }
 
 }
