142d4eb1c6c902b13df82d0f0ba593ef3347862a
abdelhafeezNada <abdelhafeez0nada@gmail.com>
2026-09-25 21:15:46 +0300

    fix requestBody import

--- src/main/java/com/example/advisorlab/controller/ChatController.java
+++ src/main/java/com/example/advisorlab/controller/ChatController.java
@@ -1,13 +1,13 @@
 package com.example.advisorlab.controller;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.advisorlab.controller.dto.ChatRequest;
 import com.example.advisorlab.service.ChatService;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
