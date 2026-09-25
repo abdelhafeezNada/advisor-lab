772b48f396a3a31da0c432f1cd5c553cbfae2be8
abdelhafeezNada <abdelhafeez0nada@gmail.com>
2026-09-25 16:45:17 +0300

    first commit

---
+++ src/main/java/com/example/advisorlab/AdvisorLabApplication.java
@@ -0,0 +1,13 @@
+package com.example.advisorlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvisorLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvisorLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/advisorlab/AdvisorLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.advisorlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvisorLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
